d548d5626fd1d92d81970a7b3c43fd28cf86fd64
John Skeen <jskeen@gmail.com>
2019-07-03 14:34:14 -0400

    Project files

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const pug = require('pug');
+const path = require('path');
+
+const routes = require('./routes/index');
+const books = require('./routes/books');
+
+// Database
+
+const db = require('./config/database');
+
+// Test DB
+db.authenticate()
+  .then(() => console.log('Database connected'))
+  .catch(err => console.log('Error: ' + err))
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/books', books);
+
+app.use(function(req, res, next){
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
---
+++ routes/books.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const router = express.Router();
+const Books = require('../models/Books');
+
+
+//GET /books - Full book list
+router.get('/', (req, res) =>
+ Books.findAll({order: [["author", "ASC"]]}).then(function(books){
+   res.render('books/index', { books: books, title: "Books" });
+ }).catch(function(err){
+   res.sendStatus(500);
+ })
+);
+
+//POST Creates a book
+router.post('/new', function(req, res, next) {
+  Books.create(req.body).then(function(book) {
+    res.redirect('/');
+  }).catch(function(err){
+    if(err.name === "SequelizeValidationError") {
+      res.render('books/new-book', {
+        book: Books.build(req.body),
+        title: "New Book",
+        errors: err.errors
+      });
+    } else {
+      throw err;
+    }
+  }).catch(function(err){
+    res.render('error', err);
+    console.log(err);
+  });
+});
+
+//GET /books/new - Shows the create book form
+router.get('/new', (req, res) =>
+  res.render('books/new-book', {book: Books.build(), title: "New Book"})
+);
+
+//GET /books/:id/update - Shows the update book form
+router.get('/:id/update', function(req, res, next){
+  Books.findByPk(req.params.id).then(function(book) {
+    if(book) {
+      res.render('books/update-book', {book: book, title: 'Edit Book'});
+    } else {
+      res.render('error', err);
+      console.log(err);
+    }
+  }).catch(function(err){
+    res.render('error', err);
+    console.log(err);
+  });
+});
+
+//POST /books/:id/delete - Deletes book from database.
+router.post('/:id/delete', function(req, res, next){
+  Books.findByPk(req.params.id).then(function(book) {
+    if(book) {
+      return book.destroy();
+    } else {
+      res.render('books/page-not-found');
+    }
+  }).then(() => {
+    res.redirect("/books");
+  }).catch(function(err){
+    res.render('error', err);
+  });
+});
+
+//POST /books/:id/update - Updates book info in the database.
+router.post('/:id/update', function(req, res, next){
+  Books.findByPk(req.params.id).then(function(book) {
+    if(book) {
+      return book.update(req.body);
+    } else {
+      res.render('error', err);
+    }
+  }).then(function(article){
+    res.redirect("/");
+  }).catch(function(err){
+    if(err.name === "SequelizeValidationError") {
+      res.render("books/update-book", {
+        book: Book.build(req.body),
+        title: "Edit Book",
+        errors: err.errors
+      });
+    } else {
+      throw err;
+    }
+  }).catch(function(err){
+    res.render('error', err)
+  });
+});
+
+module.exports = router;
